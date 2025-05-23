30cb4272d9c1556c491f48730617651ccc7ca9e1
X3s3nt1a9 <x3n1x@mail.ru>
2025-05-23 10:44:39 +0500

    first commit

---
+++ scroll.js
@@ -0,0 +1,15 @@
+function changeNavigationOnScroll() {
+    let scrollPos = window.scrollY;
+    const desktopNav = document.querySelector('.desktop-navigation');
+    const desktopNavFixed = document.querySelector('.desktop-navigation-fixed');
+
+    if (scrollPos > 75) {
+        if (desktopNav) desktopNav.classList.add('hidden');
+        if (desktopNavFixed) desktopNavFixed.style.display = 'block';
+    } else {
+        if (desktopNavFixed) desktopNavFixed.style.display = 'none';
+        if (desktopNav) desktopNav.classList.remove('hidden');
+    }
+}
+
+window.addEventListener('scroll', changeNavigationOnScroll);
